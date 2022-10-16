1722c2aa48b3fd8ba1e25e8fb77f5976d46c3e0a
JMarques8 <joana893.marques@gmail.com>
2022-10-16 14:47:15 +0100

    just a few updates

---
+++ src/main/java/com/JMarques8/hero/Game.java
@@ -0,0 +1,31 @@
+package com.JMarques8.hero;
+
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+
+import java.io.IOException;
+
+public class Game {
+
+    private Screen screen;
+
+    public Game(){
+        try {
+            TerminalSize terminalSize = new TerminalSize(40, 20);
+            DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory().setInitialTerminalSize(terminalSize);
+            Terminal terminal = terminalFactory.createTerminal();
+            Screen screen = new TerminalScreen(terminal);
+            screen.setCursorPosition(null); // we don't need a cursor
+            screen.startScreen(); // screens must be started
+            screen.doResizeIfNecessary(); // resize screen if necessary
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
